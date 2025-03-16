8a77d9f3694db65bb1e9b32f01491e85424ced67
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2025-03-16 14:28:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //your JS code here. If required.
-const line = document.getElementById('line');
-let angle = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    let line = document.getElementById("line");
+    let angle = 0;
 
-setInterval(() => {
-    angle += 2;
-    line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
-}, 20);
+    setInterval(() => {
+        angle += 2;
+        line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+    }, 20);
+});
